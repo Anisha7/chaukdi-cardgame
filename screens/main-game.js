45c7fcb4da21a7d5d290d4079ba91cacbd4d7e03
Anisha Jain <anishajain@outlook.com>
2019-05-14 02:00:04 -0700

    Players can play card and change turns!

--- screens/main-game.js
+++ screens/main-game.js
@@ -14,7 +14,9 @@ class GameScreen extends Component {
             currentPlay: [],
             team1Score: 0,
             team2Score: 0,
+            turnIndex: 1,
         }
+        
     }
 
     playerCards() {
@@ -34,15 +36,47 @@ class GameScreen extends Component {
         // return stuff
     }
 
+    setCard(players) {
+        // which card was selected: change turn
+        const selectedCard = this.props.navigation.getParam('selectedCard', null)
+        console.log(selectedCard)
+        // ADD TO IF: SELECTED CARD IS NOT ALREADY IN CURRENT PLAY
+        if (selectedCard !== null && this.state.currentPlay.includes(selectedCard) !== true) {
+            this.state.currentPlay.push(selectedCard) // works
+            // update turn index
+            console.log("HERE")
+            if (this.state.turnIndex == 4) {
+                console.log("PAST INDEX LIMIT")
+                this.setState({turnIndex: 1})
+            } else {
+                console.log("UPDATING TURN INDEX")
+                let newIndex = this.state.turnIndex + 1
+                // this.setState({turnIndex: newIndex}) // not working
+                this.state.turnIndex = newIndex
+                console.log(this.state.turnIndex)
+            }
+            let item = players.filter(({player}) => player == this.state.turnIndex)[0]
+            console.log(item[0])
+            this.setState({turn: item.name})
+        }
+    }
+
     render() {
         // access store players array
         console.log("IN GAME:")
         // console.log(this.props.players)
         const { navigate } = this.props.navigation
+        const { players } = this.props
         if (this.state.turn == '') {
-            this.state.turn = this.props.players.filter(({player}) => player == 1)[0].name
+            // this.state.turn = players.filter(({player}) => player == 1)[0].name
+            let item = players.filter(({player}) => player == this.state.turnIndex)[0]
+            console.log(item[0])
+            this.setState({turn: item.name})
+
         }
 
+        this.setCard(players)
+        
         return (
             <View>
                 {/* display player name/team and the card they played */}
@@ -56,6 +90,9 @@ class GameScreen extends Component {
                     // <3
                     onPress={() => navigate('PlayerHand', { turn: this.state.turn })}
                 />
+
+                <Text> CURRENT PLAY </Text>
+                <Text> {this.state.currentPlay.join()}</Text>
             </View>
         )
     }
--- screens/player-hand.js
+++ screens/player-hand.js
@@ -66,6 +66,7 @@ class PlayerHand extends Component {
     }
 
     render(){
+        const { navigate } = this.props.navigation
         console.log("PLAYER HAND PAGE")
         const data = this.getData()
         console.log(data)
@@ -90,23 +91,35 @@ class PlayerHand extends Component {
 
                 </ScrollView>
                 {/* form for picking a card */}
-                <Picker
+                <View style={{height: 300, width: 200}}><Picker
                     selectedValue={this.state.selectedCard}
                     style={styles.picker}
                     onValueChange={(itemValue, itemIndex) =>
                         this.setState({selectedCard: itemValue})
                     }>
                     {this.createCardForm(hand)}
-                </Picker>
+                </Picker></View>
 
                 {/* button to play card */}
                 <TouchableHighlight
                     style={styles.button}
                     onPress={() => {
                       // send played card to game screen
+                      navigate('Game', {selectedCard: this.state.selectedCard})
                     }}
                 >
                     <Text style={styles.buttonText}> Play Card </Text>
+                </TouchableHighlight>
+
+                {/* View Game Screen Again */}
+                <TouchableHighlight
+                    style={styles.button}
+                    onPress={() => {
+                      // send played card to game screen
+                      navigate('Game', {selectedCard: null})
+                    }}
+                >
+                    <Text style={styles.buttonText}> View Current Game Play </Text>
                 </TouchableHighlight>
             </View>
         )
@@ -122,16 +135,19 @@ const styles = StyleSheet.create({
     },
     picker: {
         alignSelf: 'center',
-        height: 50, 
+        height: 150, 
         width: 100,
+        margin: 20,
     },
     button: {
         padding: 20,
         borderColor: 'black',
         borderStyle: 'solid',
         borderWidth: 2,
+        justifyContent: 'flex-end',
+        marginTop: 30,
     },
-        buttonText: {
+    buttonText: {
         fontSize: 24
     },
     
